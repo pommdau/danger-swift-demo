0301c73404d69f19902b05e1f57f0b83b5ed21c2
IKEH1024 <pondau1024@gmail.com>
2022-11-18 10:35:37 +0900

    add sample iOS project

---
+++ DangerSwiftDemo/DangerSwiftDemo/DangerSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerSwiftDemoApp.swift
+//  DangerSwiftDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct DangerSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
